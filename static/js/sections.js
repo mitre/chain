a30c2cf06bc55fd2dd7f0ae23810db14b71b7eb8
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-06-28 09:54:12 -0400

    ensuring facts are sent through data_svc (#39)

--- static/js/sections.js
+++ static/js/sections.js
@@ -110,6 +110,25 @@ $(document).ready(function () {
     $('#factTbl').DataTable({})
 });
 
+function handleFactAdd(){
+    let property = document.getElementById("factProperty").value;
+    if(!property){alert('Please enter a property'); return; }
+    let value = document.getElementById("factValue").value;
+    if(!value){alert('Please enter a value'); return; }
+    let source = document.getElementById("factSource").value;
+    if(!source){alert('Please enter a source'); return; }
+
+    let facts = {
+        "index":"core_fact",
+        "property":property,
+        "value":value,
+        "source_id":source,
+        "blacklist":document.getElementById("factBlacklist").value,
+        "score":document.getElementById("factScore").value
+    };
+    restRequest('PUT', facts, alertCallback);
+}
+
 /** OPERATIONS **/
 
 let atomic_interval = null;
