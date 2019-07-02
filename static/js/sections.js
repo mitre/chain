c9e2a1d6125abfd67194f113825eee485373c672
amanners <amanners@mitre.org>
2019-07-02 13:04:53 -0400

    fixed delete and add form pages

--- static/js/sections.js
+++ static/js/sections.js
@@ -630,7 +630,11 @@ function buildRequirements(encodedTest){
     return [];
 }
 
-function checkGpsFormValid(){
+function checkGpsDeleteFormValid() {
+    validateFormState(($('#groupName').prop('selectedIndex') !== 0), '#deleteGroupBtn');
+}
+
+function checkGpsAddFormValid(){
     validateFormState(($('#groupNewName').val()), '#addGroupBtn');
 }
 
