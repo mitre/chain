981f6efe6b8f6b4fccbc9f7e577d6a586f8b3309
wbooth <18699738+wbooth@users.noreply.github.com>
2019-07-20 17:36:50 -0400

    Update sections.js
    fix bug where group dropdown can change on agent refresh

--- static/js/sections.js
+++ static/js/sections.js
@@ -113,29 +113,37 @@ function agent_refresh(){
 }
 
 function reloadGroupElements(data){
-    removeGroupElements("qgroup-");
-    addGroupElements(data, "#queueGroup", "qgroup-");
-    removeGroupElements("ggroup-");
-    addGroupElements(data, "#groupName", "ggroup-");
+    removeGroupElements(data,"qgroup-");
+    addGroupElements("#queueGroup", "qgroup-");
+    removeGroupElements(data,"ggroup-");
+    addGroupElements("#groupName", "ggroup-");
 }
 
-function addGroupElements(data, groupElementId, optionIdPrefix) {
+function addGroupElements(groupElementId, optionIdPrefix) {
     let group_elem = $(groupElementId);
     $.each(data, function(index, gp) {
         if(!group_elem.find('option[value="'+gp.id+'"]').length > 0){
-            if (gp.deactivated == 0){
+            if (gp.deactivated === 0){
                 group_elem.append("<option id='" + optionIdPrefix + gp.name + "' value='" + gp.id + "'>" + gp.name + "</option>");
             }
         }
     });
 }
 
-function removeGroupElements(optionIdPrefix) {
+function removeGroupElements(data, optionIdPrefix) {
      let options = document.querySelectorAll('*[id^="' + optionIdPrefix + '"]');
      Array.prototype.forEach.call(options, function (node) {
-         node.parentNode.removeChild(node);
+         let remove = true;
+         data.forEach(function (item, index){
+             if (node.innerText === item['name']){
+                 remove = false;
+             }
+         });
+         if(remove){
+            node.parentNode.removeChild(node);
+        }
      });
- }
+}
 
 function toggleGroupView(){
     $('#createGroupSection').toggle();
