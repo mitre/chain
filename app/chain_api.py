8055852d03a7e92a36f709e3ba761fe91c20d08f
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-08-12 08:23:52 -0400

    Revert "Tweaks to support multiple tactics per technique/id (#103)" (#104)
    This reverts commit f7a58ad8da5060bfb53c704725029ddf8cc62d07.

--- app/chain_api.py
+++ app/chain_api.py
@@ -16,7 +16,7 @@ class ChainApi:
     async def landing(self, request):
         await self.auth_svc.check_permissions(request)
         abilities = await self.data_svc.explode_abilities()
-        tactics = set([tactic for sublist in [eval(a['technique']['tactic']) for a in abilities] for tactic in sublist])
+        tactics = {a['technique']['tactic'] for a in abilities}
         hosts = await self.data_svc.explode_agents()
         groups = list(set(([h['host_group'] for h in hosts])))
         adversaries = await self.data_svc.explode_adversaries()
