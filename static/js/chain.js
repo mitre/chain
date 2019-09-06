7aa4e2f42ecf6216e6090de50bfa99322920d205
david <davidhunt@mitre.org>
2019-09-06 14:08:22 -0400

    moving shared resources to gui

--- static/js/chain.js
+++ static/js/chain.js
@@ -9,24 +9,8 @@ function clearWorkflow(){
     $('.section-profile').each(function(){ $(this).css('display', 'none'); });
 }
 
-function restRequest(type, data, callback, endpoint='/plugin/chain/rest') {
-    $.ajax({
-       url: endpoint,
-       type: type,
-       contentType: 'application/json',
-       data: JSON.stringify(data),
-       success: function(data, status, options) { callback(data); },
-       error: function (xhr, ajaxOptions, thrownError) { console.log(thrownError) }
-    });
-}
-
 function showHide(show, hide) {
     $(show).each(function(){$(this).prop('disabled', false).css('opacity', 1.0)});
     $(hide).each(function(){$(this).prop('disabled', true).css('opacity', 0.5)});
 }
 
-function updateButtonState(selector, state) {
-    (state === 'valid') ?
-        $(selector).attr('class','button-success atomic-button') :
-        $(selector).attr('class','button-notready atomic-button');
-}
