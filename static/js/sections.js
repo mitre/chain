a69c2ac0acb4ccd164c100da008a42f823204538
david <49954156+privateducky@users.noreply.github.com>
2019-09-06 14:08:52 -0400

    moving shared resources to gui (#141)

--- static/js/sections.js
+++ static/js/sections.js
@@ -532,15 +532,9 @@ function checkOpformValid(){
         '#opBtn');
 }
 
-function validateFormState(conditions, selector){
-    (conditions) ?
-        updateButtonState(selector, 'valid') :
-        updateButtonState(selector, 'invalid');
-}
-
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
